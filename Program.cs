ac633986720e1413c6c5f7076c1d27b0313aca42
Vaidas Sirtautas <v@vsshs.com>
2016-01-15 16:44:06 +0100

    Code added

--- Program.cs
+++ Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,6 +11,26 @@ namespace ClearTxtFiles
     {
         static void Main(string[] args)
         {
+            var dir = AppDomain.CurrentDomain.BaseDirectory;
+            Console.WriteLine(dir);
+
+            var dirInfo = new DirectoryInfo(dir);
+
+            var files = dirInfo.GetFiles("*.txt");
+
+            foreach (var fileInfo in files)
+            {
+                try
+                {
+                    Console.WriteLine("Clearing: " + fileInfo.Name);
+                    System.IO.File.WriteAllText(fileInfo.FullName, string.Empty);
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine("   ERROR: " + e.Message);
+                }
+
+            }
         }
     }
 }
